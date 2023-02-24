69fe7502c34fb10f15117e7e659c1b7e4d4bcf5f
Seikida <seikida@hotmail.com>
2023-02-24 15:50:52 +0900

    rollback

--- lib/ci-cd-aws-pipeline-demo1-stack.ts
+++ lib/ci-cd-aws-pipeline-demo1-stack.ts
@@ -5,14 +5,6 @@ import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/p
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { MyPipelineAppStage } from './stage';
 
-/////
-
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as iam from 'aws-cdk-lib/aws-iam';
-// import { KeyPair } from 'cdk-ec2-key-pair';
-import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-import * as path from 'path';
-
 export class CiCdAwsPipelineDemo1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -24,7 +16,6 @@ export class CiCdAwsPipelineDemo1Stack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
-    /*
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
@@ -36,6 +27,9 @@ export class CiCdAwsPipelineDemo1Stack extends cdk.Stack {
         ]
       })
     });
+
+
+
 
     const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
       env: { account: "723033854254", region: "ap-northeast-1" }
@@ -53,82 +47,6 @@ export class CiCdAwsPipelineDemo1Stack extends cdk.Stack {
     const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
       env: { account: "723033854254", region: "ap-northeast-1" }
     }));
-    */
-
-
-
-
-
-
-
-
-    //// https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ec2-instance/lib/ec2-cdk-stack.ts
-
-    // Create new VPC with 2 Subnets
-    const vpc = new ec2.Vpc(this, 'VPC', {
-      natGateways: 0,
-      subnetConfiguration: [{
-        cidrMask: 24,
-        name: "asterisk",
-        subnetType: ec2.SubnetType.PUBLIC
-      }]
-    });
-
-
-    // Allow SSH (TCP Port 22) access from anywhere
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
-      vpc,
-      description: 'Allow SSH (TCP port 22) in',
-      allowAllOutbound: true
-    });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
-
-
-    const role = new iam.Role(this, 'ec2Role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
-    })
-
-    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
-
-
-    // Use Latest Amazon Linux Image - CPU Type ARM64
-    const ami = new ec2.AmazonLinuxImage({
-      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      cpuType: ec2.AmazonLinuxCpuType.ARM_64
-    });
-
-
-    // Create the instance using the Security Group, AMI, and KeyPair defined in the VPC created
-    const ec2Instance = new ec2.Instance(this, 'Instance', {
-      vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: ami,
-      securityGroup: securityGroup,
-      // keyName: key.keyPairName,
-      role: role
-    });
-
-
-    // Create an asset that will be used as part of User Data to run on first load
-    const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../src/config.sh') });
-    const localPath = ec2Instance.userData.addS3DownloadCommand({
-      bucket: asset.bucket,
-      bucketKey: asset.s3ObjectKey,
-    });
-
-
-    ec2Instance.userData.addExecuteFileCommand({
-      filePath: localPath,
-      arguments: '--verbose -y'
-    });
-    asset.grantRead(ec2Instance.role);
-
-
-    // Create outputs for connecting
-    new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
-    // new cdk.CfnOutput(this, 'Key Name', { value: key.keyPairName })
-    new cdk.CfnOutput(this, 'Download Key Command', { value: 'aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem' })
-    new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp })
 
 
   }
